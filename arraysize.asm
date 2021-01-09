af6f2bcf0f6d84d518b3540572822481aeddfec1
AmiirNabil <76922078+AmiirNabil@users.noreply.github.com>
2021-01-09 03:06:41 +0200

    Update arraysize.asm

--- arraysize.asm
+++ arraysize.asm
@@ -3,7 +3,8 @@
 
  .DATA
     PROMPT_1  DB  'Enter Array size :',0DH,0AH,'$'
-    PROMPT_2  Dw  'The Array elements are : $'
+    PROMPT_2  Dw  'The Array elements are : $'  
+    PROMPT_3  DW  'array size cant be negative, please enter a + number'
     ARRAY DW 100 DUP(?)
 
 
@@ -22,17 +23,10 @@
 
      LEA SI, ARRAY                ; set SI=offset address of ARRAY
 
-     CALL INDECIMAL              ; call the procedure INDECIMAL Which reads the size of the array and stores it in AL
-     
-
-;---------------------------------------------------------------------------------------------------
-;----------------------------------------------------------------------------------------------------
-;the array size element is stored in AL register at this point which is 8bit in size, for further edits 
-
-
+     CALL Array_Size_Procedure              ; call the procedure READ_Size
      
      
-     AND AX, 00FFH            ; to put zeros in AH and to be able to put the counter BX = AX ( BX= 0000 AL )
+     AND AX, 00FFH 
      
      MOV BX,AX                  ; to get the value of only AL as the size of array
    
@@ -43,6 +37,17 @@
      CALL READ_ARRAY             ; call the procedure READ_ARRAY
 
 
+;-----------------------------------------------------------
+;-----------------------------------------------------------------------------------------------------
+;-----------------------------------------------------------------------------------------------------
+;---------------------------------------------------------------------------------------------------
+;----------------------------------------------------------------------------------------------------
+;the array size element is stored in AL register at this point which is 8bit in size, for further edits 
+;
+;
+;
+;
+;
 
 
      MOV AH, 4CH                  ;AH Value for dos interrupt exit program
@@ -252,7 +257,176 @@
 
    RET                            ; return control to the calling procedure
    READ_ARRAY ENDP
+   
+   ;------------------------------------------------------------------------------
+   ;------------------------------------------------------------------------------
+   ;---------------------------------------------------------------------------------
+   ;---------------------------------------------------------------------------------
+   
+   
+   
+    Array_Size_Procedure PROC
+        ; this procedure will read the array size from the user as a positive number between 0 and 255
+    
+   PUSH BX                        ; push BX onto the STACK
+   PUSH CX                        ; push CX onto the STACK
+   PUSH DX                        ; push DX onto the STACK
 
+   JMP @READ1                      ; jump to label @READ
 
+   @SKIP_BACKSPACE1:               ; jump label
+   MOV AH, 2                      ; set output function
+   MOV DL, 20H                    ; set DL=' '
+   INT 21H                        ; print a character
+
+   @READ1:                         ; jump label
+   XOR BX, BX                     ; clear BX
+   XOR CX, CX                     ; clear CX
+   XOR DX, DX                     ; clear DX
+
+   MOV AH, 1                      ; set input function
+   INT 21H                        ; read a character
+
+   CMP AL, "-"                    ; compare AL with "-"
+   JE @MINUS1                      ; jump to label @MINUS if AL="-"
+
+   CMP AL, "+"                    ; compare AL with "+"
+   JE @PLUS1                       ; jump to label @PLUS if AL="+"
+
+   JMP @SKIP_INPUT1                ; jump to label @SKIP_INPUT
+
+   @MINUS1:                        ; jump label
+   MOV CH, 1                      ; set CH=1
+   INC CL                         ; set CL=CL+1
+   JMP @error1                     ; jump to label @Error1
+   
+   @PLUS1:                         ; jump label
+   MOV CH, 2                      ; set CH=2
+   INC CL                         ; set CL=CL+1
+
+   @INPUT1:                        ; jump label
+     MOV AH, 1                    ; set input function
+     INT 21H                      ; read a character
+
+     @SKIP_INPUT1:                 ; jump label
+
+     CMP AL, 0DH                  ; compare AL with CR
+     JE @END_INPUT1                ; jump to label @END_INPUT
+
+     CMP AL, 8H                   ; compare AL with 8H
+     JNE @NOT_BACKSPACE1           ; jump to label @NOT_BACKSPACE if AL!=8
+
+     CMP CH, 0                    ; compare CH with 0
+     JNE @CHECK_REMOVE_MINUS1      ; jump to label @CHECK_REMOVE_MINUS if CH!=0
+
+     CMP CL, 0                    ; compare CL with 0
+     JE @SKIP_BACKSPACE1           ; jump to label @SKIP_BACKSPACE if CL=0
+     JMP @MOVE_BACK1               ; jump to label @MOVE_BACK
+
+     @CHECK_REMOVE_MINUS1:         ; jump label
+
+     CMP CH, 1                    ; compare CH with 1
+     JNE @CHECK_REMOVE_PLUS1       ; jump to label @CHECK_REMOVE_PLUS if CH!=1
+
+     CMP CL, 1                    ; compare CL with 1
+     JE @REMOVE_PLUS_MINUS1        ; jump to label @REMOVE_PLUS_MINUS if CL=1
+
+     @CHECK_REMOVE_PLUS1:          ; jump label
+
+     CMP CL, 1                    ; compare CL with 1
+     JE @REMOVE_PLUS_MINUS1        ; jump to label @REMOVE_PLUS_MINUS if CL=1
+     JMP @MOVE_BACK1               ; jump to label @MOVE_BACK
+
+     @REMOVE_PLUS_MINUS1:          ; jump label
+       MOV AH, 2                  ; set output function
+       MOV DL, 20H                ; set DL=' '
+       INT 21H                    ; print a character
+
+       MOV DL, 8H                 ; set DL=8H
+       INT 21H                    ; print a character
+
+       JMP @READ1                  ; jump to label @READ
+                                  
+     @MOVE_BACK1:                  ; jump label
+
+     MOV AX, BX                   ; set AX=BX
+     MOV BX, 10                   ; set BX=10
+     DIV BX                       ; set AX=AX/BX
+
+     MOV BX, AX                   ; set BX=AX
+
+     MOV AH, 2                    ; set output function
+     MOV DL, 20H                  ; set DL=' '
+     INT 21H                      ; print a character
+
+     MOV DL, 8H                   ; set DL=8H
+     INT 21H                      ; print a character
+
+     XOR DX, DX                   ; clear DX
+     DEC CL                       ; set CL=CL-1
+
+     JMP @INPUT1                   ; jump to label @INPUT
+
+     @NOT_BACKSPACE1:              ; jump label
+
+     INC CL                       ; set CL=CL+1
+
+     CMP AL, 30H                  ; compare AL with 0
+     JL @ERROR1                    ; jump to label @ERROR if AL<0
+
+     CMP AL, 39H                  ; compare AL with 9
+     JG @ERROR1                    ; jump to label @ERROR if AL>9
+
+     AND AX, 000FH                ; convert ascii to decimal code
+
+     PUSH AX                      ; push AX onto the STACK
+
+     MOV AX, 10                   ; set AX=10
+     MUL BX                       ; set AX=AX*BX
+     MOV BX, AX                   ; set BX=AX
+
+     POP AX                       ; pop a value from STACK into AX
+
+     ADD BX, AX                   ; set BX=AX+BX
+     JS @ERROR1                    ; jump to label @ERROR if SF=1
+   JMP @INPUT1                     ; jump to label @INPUT
+
+   @ERROR1:                        ; jump label
+
+   MOV AH, 2                      ; set output function
+   MOV DL, 7H                     ; set DL=7H
+   INT 21H                        ; print a character
+
+   XOR CH, CH                     ; clear CH
+
+   @CLEAR1:                        ; jump label
+     MOV DL, 8H                   ; set DL=8H   (backspace in ascii)
+     INT 21H                      ; print a character
+
+     MOV DL, 20H                  ; set DL=' '         (Space in ascii)
+     INT 21H                      ; print a character
+
+     MOV DL, 8H                   ; set DL=8H          (backspace in ascii)
+     INT 21H                      ; print a character
+   LOOP @CLEAR1                    ; jump to label @CLEAR if CX!=0
+
+   JMP @READ1                      ; jump to label @READ
+
+   @END_INPUT1:                    ; jump label
+
+   CMP CH, 1                      ; compare CH with 1   
+   JNE @EXIT1                      ; jump to label @EXIT if CH!=1
+   NEG BX                         ; negate BX
+
+   @EXIT1:                         ; jump label
+
+   MOV AX, BX                     ; set AX=BX
+
+   POP DX                         ; pop a value from STACK into DX
+   POP CX                         ; pop a value from STACK into CX
+   POP BX                         ; pop a value from STACK into BX
+
+   RET                            ; return control to the calling procedure 
+   Array_Size_Procedure ENDP
 
  END MAIN
