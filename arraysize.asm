1047aabb803285c4e2e28c17dcc79795a1ad9705
Amir Nabil <76922078+AmiirNabil@users.noreply.github.com>
2021-01-10 04:58:23 +0200

    Update arraysize.asm

--- arraysize.asm
+++ arraysize.asm
@@ -8,7 +8,7 @@
     PROMPT_4  DB  'please,choice Array type for sort (enter 1 for Bubble sort) OR (enter 2 for Quick sort) :',0DH,0AH,'$'  
     
     ARRAY DW 100 DUP(?)
-    ARRAY_Size   DW  1 DUP(0)                      ;defines the array size as an array of one element
+
 
  .CODE
    MAIN PROC
@@ -69,146 +69,164 @@
 ;------------------------------------------------------------------------ 
  CONDITION PROC 
    
-    @READ0: 
-        xor cx,cx
-        xor bx,bx 
-        xor ax,ax
+   @READ0: 
+xor cx,cx
+xor bx,bx 
+xor ax,ax
 
-        Mov AH,1                        ;read a character
-        INT 21h                         ;interrupt dos
+Mov AH,1                        ;read a character
+INT 21h                         ;interrupt dos that takes input from user
      
-        cmp    al, '-'                  ;compare input with -
-        JE @NEGATIVE0    
-        cmp al,'+'                      ;compare input with +
-        jE    @POSITIVE0        
+cmp    al, '-'                  ;compare input with -
+JE @NEGATIVE0    
+cmp al,'+'                      ;compare input with +
+jE    @POSITIVE0        
+jmp @input0              
               
-              
-    @CHECK_BUBBLE0:  
-
-         cmp AL,31h
-         je @BUBBLE_SORT0
-         jne @CHECK_QUICK0 
-        
- 
- 
-    @CHECK_QUICK0:
-        cmp AL,32h
-        je @QUICK_SORT0 
-
-        MOV CX,1
-        Jne @ERROR0
-
-    @BUBBLE_SORT0:
-       
-        mov ah,4ch
-        int 21h
-    
 
 
-    @QUICK_SORT0:
-        mov ah,4ch
-        int 21h   
+
    
    
-    @NEGATIVE0:
-        INC CL
-        JMP @ERROR0
+@NEGATIVE0:
+INC CL
+JMP @ERROR0
    
    
-    @POSITIVE0:
-        INC CL
-        JMP @ERROR0   
+@POSITIVE0:
+INC CL
+JMP @ERROR0   
    
    
               
-    @INPUT0:                        ; jump label
-        MOV AH, 1                    ; set input function
-        INT 21H                      ; read a character
+@INPUT0:
+mov bl,al
+                        ; jump label
+     MOV AH, 1                    ; set input function
+     INT 21H                      ; read a character
 
-    @SKIP_INPUT0:                 ; jump label
+@SKIP_INPUT0:                 ; jump label
 
-        CMP AL, 0DH                  ; compare AL with Return (enter)
-        JE @END_INPUT0                ; jump to label @END_INPUT
+     CMP AL, 0DH                  ; compare AL with Return (enter)
+     JE @END_INPUT0                ; jump to label @END_INPUT
 
-        CMP AL, 8H                   ; compare AL with 8H   (backspace)
-        JNE @NOT_BACKSPACE0           ; jump to label @NOT_BACKSPACE if AL!=8
+     CMP AL, 8H                   ; compare AL with 8H   (backspace)
+     JNE @NOT_BACKSPACE0           ; jump to label @NOT_BACKSPACE if AL!=8
  
-                       
-   
-    @MOVE_BACK0:                  ; jump label
+ 
 
-        MOV AX, BX                   ; set AX=BX
-        MOV BX, 10                   ; set BX=10
-        DIV BX                       ; set AX=AX/BX
 
-        MOV BX, AX                   ; set BX=AX
 
-        MOV AH, 2                    ; set output function
-        MOV DL, 20H                  ; set DL=' '
-        INT 21H                      ; print a character
 
-        MOV DL, 8H                   ; set DL=8H
-        INT 21H                      ; print a character
 
-        XOR DX, DX                   ; clear DX
-        DEC CL                       ; set CL=CL-1
-
-        JMP @INPUT0                   ; jump to label @INPUT
-
-    @NOT_BACKSPACE0:              ; jump label
-
-        INC CL                       ; set CL=CL+1
-
-        CMP AL, 30H                  ; compare AL with 0
-        JL @ERROR0                    ; jump to label @ERROR if AL<0
-
-        CMP AL, 39H                  ; compare AL with 9
-        JG @ERROR0                   ; jump to label @ERROR if AL>9
-
-        AND AX, 000FH                ; convert ascii to decimal code
-
-        PUSH AX                      ; push AX onto the STACK
-
-        MOV AX, 10                   ; set AX=10
-        MUL BX                       ; set AX=AX*BX
-        MOV BX, AX                   ; set BX=AX
-
-        POP AX                       ; pop a value from STACK into AX
-
-        ADD BX, AX                   ; set BX=AX+BX
-        JS @ERROR0                    ; jump to label @ERROR if SF=1
-        JMP @INPUT0                     ; jump to label @INPUT
-     
                          
-    @ERROR0:                        ; jump label
+   
+@MOVE_BACK0:                  ; jump label
 
-        MOV AH, 2                      ; set output function
-        MOV DL, 7H                     ; set DL=7H
-        INT 21H                        ; print a character
+     MOV AX, BX                   ; set AX=BX
+     MOV BX, 10                   ; set BX=10
+     DIV BX                       ; set AX=AX/BX
+
+     MOV BX, AX                   ; set BX=AX
+
+     MOV AH, 2                    ; set output function
+     MOV DL, 20H                  ; set DL=' '
+     INT 21H                      ; print a character
+
+     MOV DL, 8H                   ; set DL=8H
+     INT 21H                      ; print a character
+
+     XOR DX, DX                   ; clear DX
+     DEC CL                       ; set CL=CL-1
+
+     JMP @INPUT0                   ; jump to label @INPUT
+
+
+
+
+
+
+        
+        
+        
+@NOT_BACKSPACE0:              ; jump label
+
+     INC CL                       ; set CL=CL+1
+
+     CMP AL, 30H                  ; compare AL with 0
+     JL @ERROR0                    ; jump to label @ERROR if AL<0
+
+     CMP AL, 39H                  ; compare AL with 9
+     JG @ERROR0                    ; jump to label @ERROR if AL>9
+
+     AND AX, 000FH                ; convert ascii to decimal code
+
+     PUSH AX                      ; push AX onto the STACK
+
+     MOV AX, 10                   ; set AX=10
+     MUL BX                       ; set AX=AX*BX
+     MOV BX, AX                   ; set BX=AX
+
+     POP AX                       ; pop a value from STACK into AX
+
+     ADD BX, AX                   ; set BX=AX+BX
+     JS @ERROR0                    ; jump to label @ERROR if SF=1
+   JMP @INPUT0                     ; jump to label @INPUT
+        
+        
+        
+        
+        
+                         
+@ERROR0:                        ; jump label
+
+   MOV AH, 2                      ; set output function
+   MOV DL, 7H                     ; set DL=7H
+   INT 21H                        ; print a character
       
       
-    @CLEAR0:                        ; jump label
-        MOV DL, 8H                   ; set DL=8H (backspace in ascii)
-        INT 21H                      ; print a character
+      @CLEAR0:                        ; jump label
+     MOV DL, 8H                   ; set DL=8H (backspace in ascii)
+     INT 21H                      ; print a character
 
-        MOV DL, 20H                  ; set DL=' '    (Space in ascii)
-        INT 21H                      ; print a character
+     MOV DL, 20H                  ; set DL=' '    (Space in ascii)
+     INT 21H                      ; print a character
 
-        MOV DL, 8H                   ; set DL=8H          (backspace in ascii)
-        INT 21H                      ; print a character
-        LOOP @CLEAR0                    ; jump to label @CLEAR if CX!=0
+     MOV DL, 8H                   ; set DL=8H          (backspace in ascii)
+     INT 21H                      ; print a character
+   LOOP @CLEAR0                    ; jump to label @CLEAR if CX!=0
 
-        JMP @READ0                      ; jump to label @READ
+   JMP @READ0                      ; jump to label @READ
        
-     @END_INPUT0:                    ; jump label
+ @END_INPUT0:                    ; jump label
+           
+           @CHECK_BUBBLE0:  
 
-        CMP CH, 1                      ; compare CH with 1   
-        JNE @EXIT0                      ; jump to label @EXIT if CH!=1
-        NEG BX                         ; negate BX
+    cmp bL,31h
+    je @BUBBLE_SORT0
+    jne @CHECK_QUICK0 
+ 
+ 
+ 
+@CHECK_QUICK0:
+            cmp bL,32h
+            je @QUICK_SORT0 
+
+            MOV CX,1
+            Jne @ERROR0
+
+
+
+@BUBBLE_SORT0:
     
-    @EXIT0:                         ; jump label
+             mov ah,4ch
+             int 21h
+    
 
-        MOV AX, BX                     ; set AX=BX
+
+@QUICK_SORT0:
+            mov ah,4ch
+            int 21h   
 
         POP DX                         ; pop a value from STACK into DX
         POP CX                         ; pop a value from STACK into CX
